2ac935a5f4de8ec0fbf53f27303da2e8dae321ad
Saba Shahbaz <sabashahbaz001@gmail.com>
2023-06-09 18:42:55 -0400

    Adds package-lock.json

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+// const people = [];
+
+function writeCards(names, event) {
+    const people = [];
+  for (let i = 0; i < names.length; i++) {
+   people.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+   
+  }
+
+  return people;
+}
+
+// writeCards(([person], "birthday"))
+
+function countDown(number) {
+    while (number > 0) {
+    console.log(number);
+    number--;
+}
+   console.log(number)
+}
